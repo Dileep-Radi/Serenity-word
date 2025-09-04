6fe563032704b237c6577b036e8c0ee842f4f933
Dileep <radidileep3@gmail.com>
2025-09-04 13:03:09 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 # Replace <username> and <password> with your MongoDB credentials
-client = MongoClient("mongodb+srv://Dileep:<db_password>@cluster0.3orsxcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://<username>:<db_password>@cluster0.3orsxcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["versefinder"]
 emotions_collection = db["emotions"]
